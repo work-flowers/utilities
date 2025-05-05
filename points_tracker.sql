91f093bb7e3c42596de154552960911f290c4cea
denchiuten <d.chiuten@icloud.com>
2025-05-05 18:13:04 +0800

    moved the QUALIFY into a CTE

--- points_tracker.sql
+++ points_tracker.sql
@@ -1,6 +1,16 @@
+WITH sub AS (
+	SELECT *
+	FROM stripe.subscription_history
+	-- we only want the most recent row for each subscription_id, since 
+	-- stripe.subscription_history is a versioned history table
+	QUALIFY ROW_NUMBER() OVER(PARTITION BY id ORDER BY created DESC) = 1
+)
+
 SELECT
 	com.company_name,
 	com.company_id,
+	proj.linear_customer_id,
+	proj.linear_project_id,
 	DATE(sub.current_period_start) AS current_period_start,
 	DATE(sub.current_period_end) AS current_period_end,
 	JSON_VALUE(pr.metadata, '$.monthly_points') AS monthly_points,
@@ -12,7 +22,7 @@ SELECT
 	DATE(i.created_at) AS linear_issue_created_at,
 	DATE(i.started_at) AS linear_issue_started_at,
 	DATE(i.completed_at) AS linear_issue_completed_at
-FROM stripe.subscription_history AS sub
+FROM sub
 
 -- find subscription line items
 INNER JOIN stripe.subscription_item AS si
@@ -48,12 +58,12 @@ LEFT JOIN google_sheets.project_ids AS proj
 
 -- find any linear issues tied to linear project and within the current Stripe billing cycle
 LEFT JOIN linear.issue AS i
-	ON (
+	ON proj.linear_project_id = i.project_id
+	AND (
 		DATE(i.started_at) BETWEEN DATE(sub.current_period_start) AND DATE(sub.current_period_end) 
 		OR DATE(i.completed_at) BETWEEN DATE(sub.current_period_start) AND DATE(sub.current_period_end)
 		OR DATE(i.created_at) BETWEEN DATE(sub.current_period_start) AND DATE(sub.current_period_end)
 	)
-	AND proj.linear_project_id = i.project_id
 
 -- finally, pull Linear status name values
 LEFT JOIN linear.workflow_state AS ws
@@ -63,6 +73,3 @@ WHERE
 	AND sub.status = 'active'
 	AND CURRENT_DATE BETWEEN DATE(sub.current_period_start) AND DATE(sub.current_period_end)
 
--- we only want the most recent row for each subscription_id, since 
--- stripe.subscription_history is a versioned history table
-QUALIFY ROW_NUMBER() OVER(PARTITION BY sub.id ORDER BY sub.created DESC) = 1
